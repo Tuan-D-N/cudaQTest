aac62a00c6fd812a2aaf7f239ee61517660d3574
Tuan Nguyen <nguyendt03@gmail.com>
2024-12-05 11:59:11 +0800

    changed number of qubits and file names

--- GroverStressTest/runGroverCPPdoubleReg.sh
+++ GroverStressTest/runGroverCPPdoubleReg.sh
@@ -7,9 +7,9 @@
 
 for repeat in {1..5}
 do
-    for qubit in $(seq 2 2 40)
+    for qubit in $(seq 10 5 60)
     do
-        srun --exclusive --gres=gpu:1 --ntasks=1 --cpus-per-task=32 ./GroverScalingTest_doubleReg_functors.o $qubit > groverResults/GroverScalingTest_doubleReg_functors_qubit${qubit}_repeat${repeat}
+        srun --exclusive --gres=gpu:1 --ntasks=1 --cpus-per-task=32 ./GroverScalingTest_doubleReg_functors.o $qubit >> groverResults/GroverScalingTest_doubleReg_functors_qubit${qubit}
     done
 done
 
--- GroverStressTest/runGroverCPPmultiCtrl.sh
+++ GroverStressTest/runGroverCPPmultiCtrl.sh
@@ -7,9 +7,9 @@
 
 for repeat in {1..5}
 do
-    for qubit in $(seq 2 2 40)
+    for qubit in $(seq 10 5 60)
     do
-        srun --exclusive --gres=gpu:1 --ntasks=1 --cpus-per-task=32 ./GroverScalingTest_multiCtrl_functors.o $qubit > groverResults/GroverScalingTest_multiCtrl_functors_qubit${qubit}_repeat${repeat}
+        srun --exclusive --gres=gpu:1 --ntasks=1 --cpus-per-task=32 ./GroverScalingTest_multiCtrl_functors.o $qubit >> groverResults/GroverScalingTest_multiCtrl_functors_qubit${qubit}
     done
 done
 

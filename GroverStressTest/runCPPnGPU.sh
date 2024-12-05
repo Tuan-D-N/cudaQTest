3c1e7ff661285818267389ad0c22387aadbf4688
Tuan Nguyen <nguyendt03@gmail.com>
2024-12-05 16:00:06 +0800

    change sbatch depedency

--- GroverStressTest/runCPPnGPU.sh
+++ GroverStressTest/runCPPnGPU.sh
@@ -3,23 +3,31 @@
 
 load_modules_for cudaq
 
-fileNm=$1
-numGPU=$2
-baseNm=$(basename "$fileNm" .o)
-resultDIR="groverResults"
+numGPU=$1 
+cppFiles=$(ls *.o)
+pythonFiles=$(ls *.py)
 
-for repeat in {1..5}
-do
-    for qubit in $(seq 10 5 20)
-    do
-        srun --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU)) ./$fileNm $qubit >> ${resultDIR}/${baseNm}_${numGPU}GPU
-    done
-done
+for fileNm in ${cppFiles}
 
-for repeat in {1..5}
-do
-    for qubit in $(seq 21 1 60)
     do
-        srun --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU)) ./$fileNm $qubit >> ${resultDIR}/${baseNm}_${numGPU}GPU
+
+    baseNm=$(basename "$fileNm" .o)
+    resultDIR="groverResults"
+
+    for repeat in {1..5}
+    do
+        for qubit in $(seq 10 5 20)
+        do
+            srun --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU)) ./$fileNm $qubit >> ${resultDIR}/${baseNm}_${numGPU}GPU
+        done
     done
+
+    for repeat in {1..5}
+    do
+        for qubit in $(seq 21 1 60)
+        do
+            srun --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU)) ./$fileNm $qubit >> ${resultDIR}/${baseNm}_${numGPU}GPU
+        done
+    done
+
 done
--- GroverStressTest/sbatchAll.sh
+++ GroverStressTest/sbatchAll.sh
@@ -1,14 +1,10 @@
 
 numGPU=$1
-cppFiles=$(ls *.o)
-pythonFiles=$(ls *.py)
 
-for file in ${cppFiles}
-do
-    sbatch runCPPnGPU.sh file ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU))
-done
 
-for file in ${pythonFiles}
-do
-    sbatch runPynGPU.sh file ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU))
-done
+
+sbatch runCPPnGPU.sh ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU))
+
+
+
+sbatch runPynGPU.sh ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU))

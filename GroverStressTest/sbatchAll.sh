e91757b6c71299a6c5d4dd2a91c259afbc65a98d
Tuan Nguyen <nguyendt03@gmail.com>
2024-12-05 16:09:01 +0800

    addedtime

--- GroverStressTest/sbatchAll.sh
+++ GroverStressTest/sbatchAll.sh
@@ -3,8 +3,8 @@ numGPU=$1
 
 
 
-sbatch runCPPnGPU.sh ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU))
+sbatch runCPPnGPU.sh ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU)) --time=10:00:00
 
 
 
-sbatch runPynGPU.sh ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU))
+sbatch runPynGPU.sh ${numGPU} --exclusive --gres=gpu:${numGPU} --ntasks=${numGPU} --cpus-per-task=$((32 * numGPU)) --time=10:00:00

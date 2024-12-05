4dcfd5887246a8d01c7b92bd6d9f7bb58c17152b
Tuan Nguyen <nguyendt03@gmail.com>
2024-12-05 11:19:24 +0800

    bug fix 1

--- GroverStressTest/runAllGroverScalingTest.sh
+++ GroverStressTest/runAllGroverScalingTest.sh
@@ -9,7 +9,7 @@ for repeat in {1..2}
 do
     for qubit in $(seq 2 2 40)
     do
-        srun --exclusive --gres=gpu:1 --ntasks=1 --cpus-per-task=32 ./GroverScalingTest_doubleReg_functors.cpp $qubit > groverResults/GroverScalingTest_doubleReg_functors_qubit${qubit}_repeat${repeat}
+        srun --exclusive --gres=gpu:1 --ntasks=1 --cpus-per-task=32 ./GroverScalingTest_doubleReg_functors.o $qubit > groverResults/GroverScalingTest_doubleReg_functors_qubit${qubit}_repeat${repeat}
     done
 done
 
